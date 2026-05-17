294119ae9cb29b939471afc41368b8b93563fd52
Saber <samirsamado5@gmail.com>
2026-05-17 13:46:01 -0700

    create update_user API method

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
-from schemas import PostResponse, PostCreate, UserResponse, UserCreate, PostUpdate
+from schemas import PostResponse, PostCreate, UserResponse, UserCreate, PostUpdate, UserUpdate
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 from typing import Annotated
@@ -154,6 +154,44 @@ def get_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    return user
+
+
+@app.patch("/api/users/{user_id}", response_model=UserResponse)
+def update_user(user_id: int, user_update: UserUpdate, db: Annotated[Session, Depends(get_db)]):
+    result = db.execute(select(models.User).where(models.User.id == user_id))
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    if user_update.username is not None and user_update.username != user.username:
+        result = db.execute(select(models.User).where(models.User.username == user_update.username))
+        existing_user = result.scalars().first()
+
+        if existing_user:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User name already exists")
+
+    if user_update.email is not None and user_update.email != user.email:
+        result = db.execute(select(models.User).where(models.User.email == user_update.email))
+        existing_email = result.scalars().first()
+
+        if existing_email:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User name already exists")
+
+    # After the validation, i can update only the fields are provided
+    if user_update.username is not None:
+        user.user_name = user_update.username
+
+    if user_update.email is not None:
+        user.email = user_update.email
+
+    if user_update.image_file is not None:
+        user.image_file = user_update.image_file
+
+    db.commit()
+    db.refresh(user)
 
     return user
 
